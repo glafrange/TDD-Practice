a4d0d31d8e49623e2ce2cdcae0bb1e19a95d7c74
Gabriel Lafrange <gabriellafrange@gmail.com>
2018-10-30 22:22:28 -0400

    Part 4

---
+++ ThirtyDaysOfTdd.UnitTests/StringUtils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThirtyDaysOfTdd.UnitTests
+{
+	class StringUtils
+	{
+		public int FindNumberOfOccurances(string sentenceToScan, string characterToScanFor)
+		{
+			var stringToCheckAsCharArray = sentenceToScan.ToCharArray();
+			var characterToCheckFor = Char.Parse(characterToScanFor);
+			var numberOfOccurances = 0;
+
+			for (var charIdx = 0; charIdx < stringToCheckAsCharArray.GetUpperBound(0); charIdx++)
+			{
+				if (stringToCheckAsCharArray[charIdx] == characterToCheckFor)
+				{
+					numberOfOccurances++;
+				}
+			}
+
+			return numberOfOccurances;
+		}
+	}
+}
---
+++ ThirtyDaysOfTdd.UnitTests/StringUtilsTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ThirtyDaysOfTdd.UnitTests
+{
+	[TestFixture]
+	public class StringUtilsTest
+	{
+		[Test]
+		public void ShouldBeAbleToCountNumberOfLettersInSimpleSentence()
+		{
+			var sentenceToScan = "TDD is awesome!";
+			var characterToScanFor = "e";
+			var expectedResult = 2;
+			var stringUtils = new StringUtils();
+
+			int result = stringUtils.FindNumberOfOccurances(sentenceToScan, characterToScanFor);
+
+			Assert.AreEqual(expectedResult, result);
+		}
+
+		[Test]
+		public void ShouldBeAbltToCountNumberOfLettersInComplexSentence()
+		{
+			var sentenceToScan = "Once is unique, twice is a coincidence, three times is a pattern.";
+			var characterToScanFor = "n";
+			var expectedResult = 5;
+			var stringUtils = new StringUtils();
+
+			int result = stringUtils.FindNumberOfOccurances(sentenceToScan, characterToScanFor);
+
+			Assert.AreEqual(expectedResult, result);
+		}
+	}
+}
